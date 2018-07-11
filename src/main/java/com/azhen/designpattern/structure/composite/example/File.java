6a977a445c64df1087aece8a437a18bed49e99ab
Azhen <az2960426127@gmail.com>
2018-07-11 21:48:54 +0800

    组合模式

---
+++ src/main/java/com/azhen/designpattern/structure/composite/example/File.java
@@ -0,0 +1,19 @@
+package com.azhen.designpattern.structure.composite.example;
+
+public abstract class File {
+    String name;
+
+    public File(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract void display();
+}
